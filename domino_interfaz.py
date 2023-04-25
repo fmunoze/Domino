eafab1ab786ac84d337f2fa0b8f02e84a8fe7394
NoGitNoLife <111093017+NoGitNoLife@users.noreply.github.com>
2023-04-25 14:57:04 -0500

    Update domino_interfaz.py

--- domino_interfaz.py
+++ domino_interfaz.py
@@ -23,132 +23,77 @@ player4 = Player('Experto',fichas[21:28],tablero)
 Player.designacion_turnos()
 
 
+##########################
+###########################
 
-pygame.init()
+import pygame
 
 
+pygame .init()
 
 
-#ventana y nombre
-pantalla = pygame.display.set_mode((900,800))
+#para los fps, no estoy seguro como va bien aún o si va o no
+pygame.time.Clock().tick(10)
+
+fichaColor = 248,246,241
+
+ancho = 1000
+alto = 700
+
+pantalla = pygame.display.set_mode((ancho,alto))
 pygame.display.set_caption("Dominó")
-#color para las fichas
-crema = 248,246,241
-#fondo
+
+
 board = pygame.image.load("marmol.png")
 boardrect = board.get_rect()
 
 
-game = True
-ganador = False
-turno = 1
+#fondoColor = (193,217,232)
+#pantalla.fill(fondoColor)
 
+celdaColor1=  (204, 224, 232)
+celdaColor2 = (218, 232, 240)
 
-while game:
+tamanoCelda = 50
+tableroPosX = 50
+tableroPosY = 50
 
+tableroAncho = tamanoCelda * 18
+tableroAlto = tamanoCelda * 12
+
+for fila in range (12):
+    for columna in range (18):
+
+        if (fila+columna)%2 == 0:
+            color = (celdaColor1)
+        else:
+            color = (celdaColor2)
+
+        pygame.draw.rect(pantalla, color, (tableroPosX + columna * tamanoCelda, tableroPosY + fila * tamanoCelda, tamanoCelda, tamanoCelda))
+
+fichasPosX = 150
+fichasPosY = 580 
+fichasAncho = 700
+fichasAlto = 110
+
+pygame.draw.rect(pantalla, (255,255,255), (fichasPosX, fichasPosY, fichasAncho, fichasAlto))
+
+while True:
     for event in pygame.event.get():
-            
-        if event.type == pygame.QUIT:  jugando = False    
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
             
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if player == player1:
-                clickPos = pygame.mouse.get_pos()
-
-    #esto es por si no hay textura, pone un fondo azul claro
-    #ventana.fill((37, 150, 190))
+            clickPos = pygame.mouse.get_pos()
 
 
-    #para poner objetos encima:
-    pantalla.blit(board,boardrect)
-    pygame.display.flip()
-
-    #para los fps, no estoy seguro como va bien aún o si va o no
-    pygame.time.Clock().tick(5)
- 
-
-
-
-    #no se
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running= False
-                pygame.quit()
+    pygame.display.update()
 
 
 
 
-
-
-
-    n_p=0
-    for player in Player.players:
-        sleep(0.5)
-
-        print('-------------------------------------------------------------')
-        
-        if player.jugadas_disponibles()==[]:
-            n_p+=1
-            print(f'\n{player.name} no tiene jugadas dispobibles\n')
-            if n_p==4:
-                ganador=player
-                for _player in Player.players:
-                    print(f'mano de {_player.name}: {_player.fichas}')
-                    if sum(list(map(sum,_player.fichas)))<sum(list(map(sum,ganador.fichas))):
-                        ganador=_player
-                print(f'\nGANADOR POR PUNTOS!:{ganador.name}')
-                game=False
-                break
-            continue
-        
-        if player!=player1:
-
-            if player == player2:
-                player.jugada_automatica(1)
-                continue
-            elif player == player3:
-                player.jugada_automatica(2)
-                continue
-
-            elif player == player4:
-                player.jugada_automatica(3)
-                continue
-
-
-
-
-        jugada_valida=False
-        print(f'Turno de {player.name}\n     Tablero: {player.tablero.fichas}\n     Mano: {player.fichas}\n     Jugadas disponibles: {player.jugadas_disponibles()}\n')
-        
-        while not jugada_valida:
-            
-            try:
-                ficha=tuple(map(int,input('ingrese la ficha a jugar de la forma \'n m\', para pasar 7 7: ').split()))
-            except:
-                print('Jugada invalida, ingrese de nuevo la ficha')
-                continue
-            
-            if ficha==(7,7):
-                print(f'{player.name} pasó')
-                jugada_valida=True  
-
-            elif ficha in player.jugadas_disponibles():
-                jugada_valida=True     
-            else:
-                print('Jugada invalida, ingrese de nuevo la ficha')
-        
-        
-        if ficha==(7,7):continue
-        
-        player.jugar_ficha(ficha)
-        
-        if player.fichas==[]:
-            print(f'GANADOR!: {player.name}')
-            game=False
-            break
-
-        
 pygame.quit()
 
 
